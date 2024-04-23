968c2c13bf0527c1b79ea1747d6941c74ca018da
AndresChCy <aichaparrom@gmail.com>
2024-04-23 15:44:31 -0400

    Añadiendo las herencias (Base de PA3P)

---
+++ Tarea1DOO/src/main/java/org/example/CocaCola.java
@@ -0,0 +1,11 @@
+package org.example;
+
+class CocaCola extends Bebida{
+    public CocaCola(int a){
+        super(a);
+    }
+    public String beber(){
+        return "cocacola";
+    }
+}
+
---
+++ Tarea1DOO/src/main/java/org/example/Sprite.java
@@ -0,0 +1,12 @@
+package org.example;
+
+class Sprite extends Bebida{
+    public Sprite(int a){
+        super(a);
+    }
+    public String beber(){
+        return "sprite";
+    }
+
+}
+
