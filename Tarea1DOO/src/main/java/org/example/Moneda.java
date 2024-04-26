008fe774c49bcf847cf55a2bd75bf6e40bcd0b6d
Bastian Bernal <bbernal2022@udec.cl>
2024-04-26 01:33:07 -0400

    Se implementa la interfaz comparable en moneda y se define un método para comparar su valor

--- Tarea1DOO/src/main/java/org/example/Moneda.java
+++ Tarea1DOO/src/main/java/org/example/Moneda.java
@@ -1,9 +1,31 @@
 package org.example;
 
+/**
+ * La clase abstracta Moneda representa una moneda genérica.
+ */
 abstract class Moneda {
+    /**
+     * Constructor vacío de la clase Moneda.
+     */
     public Moneda(){}
+    /**
+     * Método para obtener el número de serie de la moneda.
+     * @return La serie de la moneda.
+     */
     public Moneda getSerie(){
         return this ;
     }
+    /**
+     * Método abstracto para obtener el valor de la moneda.
+     * @return El valor de la moneda.
+     */
     public abstract int getValor();
+    /**
+     * Compara la moneda con un valor dado.
+     * @param precio El precio a comparar.
+     * @return Un entero que indica la diferencia entre el valor de la moneda y el precio dado.
+     */
+    public int comparaMoneda(int precio) {
+        return this.getValor() - precio;
+    }
 }
