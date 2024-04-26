1702a56dcd3d1a6b0d53f2a3ae681c5ee792364d
Bastian Bernal <bbernal2022@udec.cl>
2024-04-26 14:35:52 -0400

    Correción de la clase moneda con respecto a un commit anterior.

--- Tarea1DOO/src/main/java/org/example/Moneda.java
+++ Tarea1DOO/src/main/java/org/example/Moneda.java
@@ -1,9 +1,32 @@
 package org.example;
 
-abstract class Moneda {
+/**
+ * Superclase abstracta que representa una moneda.
+ */
+abstract class Moneda implements Comparable<Integer>{
+    /**
+     * Constructor por defecto de la clase Moneda.
+     */
     public Moneda(){}
+    /**
+     * Método para obtener la serie de la moneda.
+     * @return La instancia actual de la moneda.
+     */
     public Moneda getSerie(){
         return this ;
     }
+    /**
+     * Método abstracto para obtener el valor de la moneda.
+     * @return El valor de la moneda.
+     */
     public abstract int getValor();
+    /**
+     * Método para comparar el valor de la moneda con un precio dado.
+     * @param valor El precio a comparar.
+     * @return Un valor negativo si el valor de la moneda es menor que el precio,
+     *         cero si son iguales, o un valor positivo si el valor de la moneda es mayor.
+     */
+    public int compareTo(int valor) {
+        return Integer.compare(this.getValor(), valor);
+    }
 }
