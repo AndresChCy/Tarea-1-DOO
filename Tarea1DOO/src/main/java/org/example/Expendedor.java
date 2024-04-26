91fc3f2b11aa0905204de713fb69c229641a5769
AndresChCy <aichaparrom@gmail.com>
2024-04-26 01:52:01 -0400

    Arreglando el metodo constructor de Expendedor para varios depositos

--- Tarea1DOO/src/main/java/org/example/Expendedor.java
+++ Tarea1DOO/src/main/java/org/example/Expendedor.java
@@ -1,25 +1,40 @@
 package org.example;
+
+import javax.swing.plaf.ProgressBarUI;
+
 /*Un expendedor al que le puedes comprar productos usando monedas*/
 class Expendedor {
 
     private int precio;
     /*Creamos un deposito para cada elemento necesario para el expendedor*/
-    private Deposito<Bebida> coca;
-    private Deposito<Bebida> sprite;
+    private Deposito<Producto> coca;
+    private Deposito<Producto> sprite;
+    private Deposito<Producto> fanta;
+    private Deposito<Producto> snickers;
+    private Deposito<Producto> super8;
     private Deposito<Moneda> monedero;
     public static final int  COCA=1;
     public static final int  SPRITE=2;
     /*Metodo constructor que rellena sus depositos para productos de forma magica*/
     public Expendedor(int a, int precioBebidas){
-        monedero = new Deposito();
-        coca = new Deposito();
-        sprite = new Deposito();
+        monedero = new Deposito<>();
+        coca = new Deposito<>();
+        sprite = new Deposito<>();
+        fanta = new Deposito<>();
+        snickers = new Deposito<>();
+        super8 = new Deposito<>();
         precio = precioBebidas;
         for(int i=0 ; i<a;i++){
-            Bebida b = new CocaCola(100+i);
-            Bebida b2 = new Sprite(200+i);
+            Producto b = new CocaCola(100+i);
+            Producto b2 = new Sprite(200+i);
+            Producto b3 = new Fanta(300+i);
+            Producto b4 = new Snickers(400+i);
+            Producto b5 = new Super8(500+i);
             coca.addObject(b);
             sprite.addObject(b2);
+            fanta.addObject(b3);
+            snickers.addObject(b4);
+            super8.addObject(b5);
         }
     }
     /*Metodo para comprar productos
